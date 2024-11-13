c0a1b2a9d392f214852c43e25a308ddf3f8a1758
Giovane Vieira <147013676+GiihVieira@users.noreply.github.com>
2024-11-13 09:02:49 -0300

    Adicionando recursos de listar matriculas

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from escola.models import Estudante, Curso
-from escola.serializers import EstudanteSerializer, CursoSerializer
-from rest_framework import viewsets
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursosSerializer, ListaMatriculasEstudantesSerializer
+from rest_framework import viewsets, generics
 
 
 # https://www.django-rest-framework.org/api-guide/viewsets/
@@ -11,3 +11,19 @@ class EstudanteViewSet(viewsets.ModelViewSet):
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']) # Filter como o nome já diz, ele filtra apenas uma informação dentro do objeto
+        return queryset
+    serializer_class = ListaMatriculasEstudantesSerializer
+
+class ListarMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursosSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListarMatriculaCurso
 from rest_framework import routers
 
 # Define a inicialização da routa, https://www.django-rest-framework.org/api-guide/routers/
 router = routers.DefaultRouter() 
 
 # Registra uma nova rota identificada pelo 'estudante', usa como paramentro de visualização o viewset definido anteriormente e nomeia como Estudantes
-router.register('estudante', EstudanteViewSet, basename='Estudantes') 
+router.register('estudantes', EstudanteViewSet, basename='Estudantes') 
 router.register('curso', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matrículas')
 
-# Define as urls para acesso das rotas, o '' vázio determina o acesso primario, ou seja, a home da aplicação
+# Define as urls para acesso das rotas, o '' vázio determina o acesso primario, ou seja, a home da aplicação.
+# as_view() determina que aquele acesso é somente de leitura
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('estudantes/<int:pk>/matriculas/', ListaMatriculaEstudante.as_view()),
+    path('cursos/<int:pk>/matriculas/', ListarMatriculaCurso.as_view()),
 ]
